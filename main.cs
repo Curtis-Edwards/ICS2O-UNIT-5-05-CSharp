861d11ffa4f4ed0845e6e461ef636329649b71ad
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-05 16:48:00 +0000

    added if statements

--- main.cs
+++ main.cs
@@ -5,19 +5,40 @@ class Program
     public static void Main(string[] args)
     {
         // This function accepts user input
-        int radius;
-        double pi = Math.PI;
-        double area;
+        int sidea;
+        int sideb;
+        int sidec;
 
-        Console.WriteLine("Please enter the radius of the circle");
+        Console.WriteLine("Please enter the Lengths of the sides on the triangle");
         Console.WriteLine("");
 
-        Console.Write("radius: ");
-        radius = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("");
-        area = (pi * radius) * radius;
+        Console.Write("Side A: ");
+        sidea = Convert.ToInt32(Console.ReadLine());
+        Console.Write("");
+        Console.Write("Side B: ");
+        sideb = Convert.ToInt32(Console.ReadLine());
+        Console.Write("");
+        Console.Write("Side C: ");
+        sidec = Convert.ToInt32(Console.ReadLine());
+        Console.Write("");
 
-        Console.WriteLine("Area: " + area.ToString("0.00") + "cm²");
+        if (sidea == sideb && sideb == sidec && sidec == sidea) 
+        {
+            Console.Write("Your triangle is an equilateral.");
+        } 
+        else if (sidea == sideb || sideb == sidec || sidec == sidea) 
+        {
+            Console.Write("Your triangle is an isosceles.");
+        } 
+        else if (sidea != sideb && sideb != sidec && sidec != sidea) 
+        {
+            Console.Write("Your triangle is an scalene.");
+        } 
+        else   
+        {
+            Console.Write("Somthing went wrong...");
+        }
+ 
 
         Console.WriteLine("\nDone.");
     }
